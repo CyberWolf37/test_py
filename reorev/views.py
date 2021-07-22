34581eac65d7c3d520fcf1266936051373a68326
CyberWolf37 <borel.loic.37@gmail.com>
2021-07-22 19:20:23 +0200

    add new change on home

--- reorev/views.py
+++ reorev/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, context, loader
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to my hosue !")
+    registered = False
+    temp = loader.get_template('home.html')
+    context = {
+        'registered': registered,
+    }
+    return HttpResponse(temp.render(context, request))
 
 def login(request):
     temp = loader.get_template('login.html')
