70795f84fe68108c511da842ac65f23a86a18a6a
CyberWolf <borel.loic.37@gmail.com>
2021-08-29 19:47:12 +0200

    add registered login sign in

--- reorev/views.py
+++ reorev/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, context, loader
 
 # Create your views here.
 def home(request):
-    registered = False
-    temp = loader.get_template('home.html')
+    registered = True
+    temp_home = loader.get_template('home.html')
+    temp_login = loader.get_template('login.html')
     context = {
         'registered': registered,
     }
-    return HttpResponse(temp.render(context, request))
+    #return HttpResponse(temp.render(context, request))
+    if registered:
+        return HttpResponse(temp_home.render(context, request))
+    else :
+        return HttpResponse(temp_login.render(context, request))
 
 def login(request):
     temp = loader.get_template('login.html')
