4a90fc2cdef97654a6175ecc64fc6576d20a8383
CyberWolf <borel.loic.37@gmail.com>
2021-09-20 14:59:53 +0200

    add new template file

--- reorev/forms.py
+++ reorev/forms.py
@@ -3,9 +3,9 @@ from django.forms import ModelForm
 
 class SignInForm(forms.Form):
     user_username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control'}),label="User name")
-    user_email = forms.EmailField(widget=forms.TextInput(attrs={'class' : 'form-control'}),label="Email address")
+    user_email = forms.EmailField(widget=forms.EmailInput(attrs={'class' : 'form-control'}),label="Email address")
     user_password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}), label="Password")
 
 class LogInForm(forms.Form):
-    user_email = forms.EmailField(widget=forms.TextInput(attrs={'class' : 'form-control'}),label="Email address")
+    user_email = forms.EmailField(widget=forms.EmailInput(attrs={'class' : 'form-control'}),label="Email address")
     user_password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control'}), label="Password")
--- reorev/views.py
+++ reorev/views.py
@@ -1,35 +1,35 @@
-from django.http.response import HttpResponseBadRequest
+from django.http.response import HttpResponseBadRequest, HttpResponseNotFound
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, context, loader
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login as login_dj
 
 from .forms import LogInForm, SignInForm
 
 # Create your views here.
 def home(request):
-    temp_home = loader.get_template('home.html')
-    temp_login = loader.get_template('login.html')
+    #temp_home = loader.get_template('home.html')
+    #temp_login = loader.get_template('login.html')
     #return HttpResponse(temp.render(context, request))
-    context = {
-        'registered': True,
-    }
-    if request.user.is_authenticated and request.user.:
-        return HttpResponse(temp_home.render(context, request))
+
+    if request.user.is_authenticated :
+        return render(request, 'blog.html')
     else :
-        return HttpResponse(temp_login.render(context, request))
+        return HttpResponseRedirect('/login/')
 
 def login(request):
+
     if request.method == 'POST':
         form = LogInForm(request.POST)
         if form.is_valid():
-            profil = authenticate(email=form.cleaned_data['user_email'], password=form.cleaned_data['user_password'])
-            if profil is None:
+            profil = authenticate(request, username=form.cleaned_data['user_email'], password=form.cleaned_data['user_password'])
+            if profil is not None:
+                login_dj(request,profil)
                 return HttpResponseRedirect('/')
             else:
-                return HttpResponseRedirect('/login/')
+                return HttpResponseNotFound()
             
         else:
             return HttpResponseBadRequest()
